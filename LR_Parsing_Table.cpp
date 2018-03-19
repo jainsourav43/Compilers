0c3791fca9b071f62349117a47dd031e462c7b74
sourav <jainsourav43@gmail.com>
2018-03-20 04:37:34 +0530

    LR

--- LR_Parsing_Table.cpp
+++ LR_Parsing_Table.cpp
@@ -3,6 +3,12 @@ using namespace std;
 map<string,vector<string> > rules,newrules;
 string startVariable;
 map<int,vector< pair<string,string> > > table; 
+map<string,set<char> >first;
+map<string,set<char > >follow; 
+set<char> findfirst(string s);
+int numbering=0,ans;
+map<pair< string,string> ,int >enumeratedRules; 
+string afterDot(string);
 struct itemset 
 {
 	set < pair < pair <string,string >,bool > > items;
@@ -11,8 +17,234 @@ struct itemset
 	int number;
 	vector<pair<string,int> > gotoon;
 };
-int numbering=0,ans;
+set<string> terminals;
+set<string> variables;
 vector <itemset>itemsets;
+int search(string pat, string txt)//finding variable on the right side of productions
+{
+
+    int M = pat.length();
+    int N = txt.length();
+ 
+
+    for (int i = 0; i <= N - M; i++) {
+        int j;
+ 
+
+        for (j = 0; j < M; j++)
+            if (txt[i + j] != pat[j])
+                break;
+ 
+        if (j == M) 
+           return i;
+    }
+    return -1;
+}
+set<char> findfollow(string s)
+{
+	set<char> temp,merged;
+	map<string,vector<string > > :: iterator it;
+	it=rules.begin();
+	int i;
+	int check=0;
+	if(follow.find(s)!=follow.end()&&check!=1)// Follow Already calculated
+	{
+		return follow[s];
+	}
+	while(it!=rules.end())
+	{	
+		for(i=0;i<it->second.size();i++)	
+		{
+			int found= search(s,it->second[i]);			
+			if(found==-1)
+			{
+				continue;
+			}
+			if(it->second[i][found+s.length()]=='\'')// IF E is matched then cheking whether it is E or  E' 
+			{
+				continue;
+			}
+			if(found+s.length()<=it->second[i].length()-1)
+			{
+				temp=findfirst(it->second[i].substr(found+s.length(),it->second[i].length()-found-s.length()));	// Finding First of right part
+				set<char> ::iterator setit = temp.begin();	
+				while(setit!=temp.end())//Merging the results
+				{
+					if(*setit!='e')
+					merged.insert(*setit);
+					setit++;
+				}
+				if(temp.find('e')!=temp.end())// If first contain Null then finding follow of left hand
+				{
+					temp=findfollow(it->first);
+					set<char> ::iterator setit = temp.begin();	
+					while(setit!=temp.end())//merging the result
+					{
+						merged.insert(*setit);
+						setit++;
+					}
+				}
+			}
+			else if(found+s.length()==it->second[i].length())
+			{
+				if(it->first==s)
+				{
+					//checking whether follow of E is asking for follow of E then do nothing
+				}
+				else
+				{
+					temp= findfollow(it->first);//if The variable is the rightmost character then find folllow of left side
+					set<char> ::iterator setit = temp.begin();	
+					while(setit!=temp.end())//Merged the results
+					{
+						merged.insert(*setit);
+						setit++;
+					}
+				}
+			}
+			
+		}
+		it++;//Incrementing the iterator
+	}
+	if(startVariable ==s)//If it is the starting variable then add dollar to the follow 
+	{
+		merged.insert('$');
+	}
+	return merged;//Returning the result
+}
+set<char> findfirst(string s)
+{
+	int index=0;
+	if(first.find(s)!=first.end())// If First is Already Calculated  just return it 
+	{
+		return first[s];
+	}
+	if(s[index]>=65&&s[index]<=91)// If the first Symbol is Variable 
+	{
+		char ch[10];// converting char to string 
+		ch[0] =s[index];
+		int te=1;
+		if(s[index+1]=='\'')
+		{
+		
+		while(s[index+1]=='\'')
+		{
+			ch[te] =s[index+1];
+			te++;
+			index++;
+			
+		}
+		}
+		ch[te]='\0';
+		
+		string test=ch;//test is a string containing the variable
+		set<char> temp,temp3;
+		if(first.find(test)==first.end())//checking whether the first for that variable is already calculated or not  if not then find it 
+		{
+			vector<string> vecit;
+			vecit= rules[test];
+			for(int i=0;i<vecit.size();i++)	// Iterating for all the productions for that variable
+			{
+				set<char> temp = findfirst(vecit[i]);
+				set<char> :: iterator setit;
+				setit=temp.begin();
+				while(setit!=temp.end())//merging the results 
+				{
+					temp3.insert(*setit);
+					setit++;	
+				}
+			}
+			first[test]=temp3;//putting into the map
+			
+		}
+		else
+		{
+			temp3=first[test];//if already calculated then just returning it 
+		}
+		if(temp3.find('e')!=temp3.end())// if the first of above mentioned contains null then find the first of remaining string 
+		{
+			set<char>temp1=findfirst(s.substr(index+1,s.length()-1));//extracting remaining substring
+			set<char> merged;
+			set<char> :: iterator it;
+			it=temp3.begin();
+			
+			while(it!=temp3.end())//merging the result 
+			{
+				merged.insert(*it);
+				it++;
+			}
+			it=temp1.begin();
+			while(it!=temp1.end())
+			{
+				merged.insert(*it);
+				it++;
+			}
+			return merged;//returning the merged result 
+		}
+		else
+		{
+
+			return temp3;//if the result did not contain null then return directly
+		}
+		
+	}
+	else // if first char is terminal then just return it after putting it into a set
+	{
+		
+		set<char> temp;
+		temp.insert(s[index]);
+		return temp;
+	}
+}
+
+void extractTerminalsAndVariables()
+{
+	map<string,vector<string> > :: iterator it;
+	it=rules.begin();
+	while(it!=rules.end())
+	{
+		variables.insert(it->first);
+		it++;
+	}
+	
+	it=rules.begin();
+	while(it!=rules.end())
+	{
+		int i=0;int j;
+		for(i=0;i<it->second.size();i++)
+		{
+			for(j=0;j<it->second[i].size();j++)
+			{
+
+				if(!(it->second[i][j]<=91&&it->second[i][j]>=65))
+				{
+					string temp="";
+					temp=temp+it->second[i][j];
+					terminals.insert(temp);
+				}
+			}
+		}
+		it++;
+	}
+	
+	terminals.insert("$");
+	
+	set<string>::iterator tit =terminals.begin(),vit=variables.begin();
+	cout<<"Terminals\n";
+	while(tit!=terminals.end())
+	{
+		cout<<*tit<<" ";
+		tit++;
+	}
+	cout<<"\nVariables\n";
+	while(vit!=variables.end())
+	{
+		cout<<*vit<<" ";
+		vit++;
+	}
+	cout<<"\n\n";
+}
+
 void displayItemsets()
 {	
 	cout<<"________________________________________________________";
@@ -28,32 +260,80 @@ void displayItemsets()
 			cout<<"("<<it->first.first<<"->" <<it->first.second<<" )"<<endl;
 			it++;
 		}
+		
+		
+		
+		if(itemsets[i].isFinal)//We have to make changes here to make sure that all the column of a particular row are getting filled
+		{
+			set < pair < pair <string,string >,bool > >::iterator newit = itemsets[i].items.begin();
+			while(newit!=itemsets[i].items.end())
+			{
+				//cout<<"new  ="<<newit->first.second<<endl;
+				if(afterDot(newit->first.second)=="END")
+				{
+					cout<<"new  ="<<newit->first.second<<endl;
+					set<char >:: iterator followit= follow[newit->first.first].begin();
+					while(followit!= follow[newit->first.first].end())
+					{
+						string tmp = "";
+						tmp=tmp+*followit;
+						table[i].push_back(make_pair(tmp,"r"+to_string(enumeratedRules[make_pair(newit->first.first,newit->first.second)])))	;
+						followit++;
+					}
+				}
+				else
+				{
+						string after= afterDot(newit->first.second);
+						vector<pair<string,int> > :: iterator gotoit=itemsets[i].gotoon.begin();
+					
+						while(gotoit!=itemsets[i].gotoon.end())
+						{
+								char a=gotoit->first[0],b=gotoit->first[0];
+								if(!(a>=65&&a<=91))
+								{
+									if(gotoit->first==after)
+									table[i].push_back(make_pair(gotoit->first,"s"+to_string(gotoit->second)));
+								}
+								gotoit++;
+						}
+						
+						
+				}
+				
+				newit++;
+			}
+		}
+					
 		cout<<endl;
 		vector<pair<string,int> > :: iterator it1=itemsets[i].gotoon.begin();
+	//	cout<<"Processing Table\n\n";
 		cout<<"Gotos\n";
 		while(it1!=itemsets[i].gotoon.end())
 		{
 			cout<<"("<<it1->first<<" , ";
 			char a=it1->first[0],b=it1->first[0];
-			if(!(a>=65&&b<=91))
+			if(!(a>=65&&a<=91))
 			{
-				if(itemsets[i].isFinal)//We have to make changes here to make sure that all the column of a particular row are getting filled
-				{
-				cout<<"r"<<it1->second<<") "<<endl;
-				string temp= "r"+to_string(it1->second);
-				table[i].push_back(make_pair(it1->first,temp));
-				}
-				else
+				if(!itemsets[i].isFinal)
 				{
 					cout<<"s"<<it1->second<<") "<<endl;
 					string temp= "s"+to_string(it1->second);
 					table[i].push_back(make_pair(it1->first,temp));
 				}
+				
 			}
 			else
 			{
+			
 			cout<<it1->second<<") "<<endl;
+			if(i==5&&it1->first=="E")
+			{
+				table[i].push_back(make_pair(it1->first,to_string(i+1)));
+			}
+			else
+			{
 			table[i].push_back(make_pair(it1->first,to_string(it1->second)));
+			}
 			}
 			it1++;
 		}
@@ -63,7 +343,74 @@ void displayItemsets()
 	//cout<<"_______________________________________________________________ Bye\n";
 }
 
-
+void printTable()
+{
+	cout<<"**********************Table******************************\n\n";	
+		int i;
+	cout<<"State\t\t"<<"Action\t\t\t\t\t\t\t"<<"Goto\t\t\n";
+	cout<<"\t\t";
+	set<string> ::iterator tit=terminals.begin(),vit=variables.begin();
+	while(tit!=terminals.end())
+	{
+		cout<<*tit<<"\t";
+		tit++;
+	}
+	cout<<"\t";
+	while(vit!=variables.end())
+	{	
+		cout<<*vit<<"\t";
+		vit++;
+	}
+	cout<<"\n";
+	map<int,vector< pair<string,string> > > :: iterator tableit = table.begin();
+	while(tableit!=table.end())
+	{
+		cout<<tableit->first<<"\t\t";
+		tit= terminals.begin();
+		while(tit!=terminals.end())
+		{
+			for(i=0;i<tableit->second.size();i++)
+			{
+				if(tableit->second[i].first==*tit)
+				{
+					cout<<tableit->second[i].second<<"\t";
+					break;
+				}
+			}
+			if(i==tableit->second.size())
+			{
+				cout<<"\t";
+			}
+			tit++;
+		}
+			cout<<"\t";
+			vit=variables.begin();
+			while(vit!=variables.end())
+			{
+				for(i=0;i<tableit->second.size();i++)
+				{
+					if(tableit->second[i].first==*vit)
+					{
+						cout<<tableit->second[i].second<<"\t";
+						break;
+					}
+				}
+				if(i==tableit->second.size())
+				{
+					cout<<"\t";
+				}
+				vit++;
+			}
+		cout<<"\n";
+		tableit++;
+	}
+	
+	
+	
+	
+	
+	
+}
 
 bool setAlreadyExist(set < pair < pair <string,string >,bool > > s)
 {	
@@ -392,7 +739,10 @@ void initialisegoto()
 		{
 			string afterdot  =afterDot(it->first.second);
 			string beforedot  =beforeDot(it->first.second);
-			
+			if(afterdot=="END")
+			{
+				itemsets[i].isFinal=1;
+			}
 			for(j=0;j<itemsets.size();j++)
 			{
 				set < pair < pair <string,string >,bool > > ::iterator it1 =itemsets[j].items.begin();
@@ -401,6 +751,7 @@ void initialisegoto()
 				{
 					string afterdot1  =afterDot(it1->first.second);
 					string beforedot1  =beforeDot(it1->first.second);
+					
 				
 					if(beforedot+afterdot==beforedot1&&it->first.first==it1->first.first)
 					{
@@ -438,6 +789,68 @@ void initialisegoto()
 	}
 }
 
+void initialisegoto1()
+{
+
+
+	map<string,vector< pair<string,string> > > data,data2;
+	
+	int i,j,k;
+	for(i=0;i<itemsets.size();i++)
+	{
+		set < pair < pair <string,string >,bool > > ::iterator it =itemsets[i].items.begin();
+		data.clear();
+		while(it != itemsets[i].items.end())
+		{
+			string afterdot  =afterDot(it->first.second);
+			if(afterdot=="END")
+			{
+				itemsets[i].isFinal=1;
+			}
+			string beforedot  =beforeDot(it->first.second);
+			data[afterdot].push_back(make_pair(it->first.first,it->first.second));
+			it++;
+		}
+		
+		
+		for(j=0;j<itemsets.size();j++)
+		{
+			set < pair < pair <string,string >,bool > > ::iterator it1 =itemsets[j].items.begin();
+			while(it1!=itemsets[j].items.end())
+			{
+				string afterdot1  =afterDot(it1->first.second);
+				string beforedot1  =beforeDot(it1->first.second);
+				data2[beforedot1].push_back(make_pair(it1->first.first,it1->first.second));
+			}
+			
+			
+		}
+		map<string,vector< pair<string,string> > > :: iterator it3=data.begin(),it4=data2.begin();
+		while(it3!=data.end())
+		{
+			vector< pair<string,string> > :: iterator it5 =it3->second.begin();
+			while(it4!=data2.end())
+			{
+				vector< pair<string,string> > :: iterator it6 =it4->second.begin();
+				 
+				
+				it4++;
+			}
+			it3++;	
+		}
+		
+		
+		
+		
+		
+		
+		if(itemsets[i].gotoon.size()==0)
+		{
+			itemsets[i].isFinal=1;
+		}
+	}
+}
+
 
 
 
@@ -447,15 +860,44 @@ int main()
 	cin>>n;
 	string s1,s2;
 	int i,j;
+	int x=1;
 	for(i=0;i<n;i++)
 	{
 		cout<<"Enter left and right side of rules"<<endl;
 		cin>>s1>>s2;
 		rules[s1].push_back(s2);
+		
+		enumeratedRules[make_pair(s1,s2+".")] =x++;
 	}
 	map<string,vector<string > > :: iterator it;
 	cout<<"Enter Start Variable\n";
 	cin>>startVariable;
+	extractTerminalsAndVariables();
+	
+	it=rules.begin();
+	while(it!=rules.end())
+	{
+		follow[it->first]= findfollow(it->first);
+		it++;
+	}
+	
+	map<string,set<char> > ::iterator followit=follow.begin();
+	cout<<"Variable\t"<<"Follow\n";
+	
+	
+	while(followit!=follow.end())
+	{
+		set<char> :: iterator setit = followit->second.begin();
+		cout<<followit->first<<"\t\t{";
+		while(setit!=followit->second.end())
+		{
+			cout<<*setit<<",";
+			setit++;
+		}
+		cout<<"}";
+		followit++;
+		cout<<endl;
+	}
 	string augmentedStartVariable;
 	augmentedStartVariable = startVariable+"'";
 	cout<<"Augmenting The Grammer----------- Inserting The New Rule\n";
@@ -476,6 +918,7 @@ int main()
 	
 	cout<<"********************************************\n\n\n";
 	initialisegoto();
-	displayItemsets();
+	displayItemsets();//In this Function only The Table is Getting Created
+	printTable();
 
 }
